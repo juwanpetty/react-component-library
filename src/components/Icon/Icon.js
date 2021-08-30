a5172e2010306f5ccf37a9353a368607496f93e5
juwanpetty <jchpetty@gmail.com>
2021-08-30 18:14:36 -0400

    Update Icon component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,18 +1,7 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import cx from 'classnames';
 import { icons } from 'assets/icons';
-
-const Svg = styled.svg`
-  display: ${(props) => (props.block ? 'block' : 'inline-block')};
-  vertical-align: middle;
-
-  shape-rendering: inherit;
-  transform: translate3d(0, 0, 0);
-`;
-
-const Path = styled.path`
-  fill: currentColor;
-`;
+import styles from './Icon.module.scss';
 
 /**
  * An Icon is a piece of visual element, but we must ensure its accessibility while using it.
@@ -21,17 +10,22 @@ const Path = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-export const Icon = ({ icon, block, ...props }) => (
-  <Svg
-    viewBox="0 0 1024 1024"
-    width="20px"
-    height="20px"
-    block={block}
-    {...props}
-  >
-    <Path d={icons[icon]} />
-  </Svg>
-);
+export const Icon = ({ icon, block, ...props }) => {
+  const classNames = cx(
+    cx(styles.icon, block ? styles.icon___block : styles.icon___inline)
+  );
+  return (
+    <svg
+      viewBox="0 0 1024 1024"
+      width="20px"
+      height="20px"
+      {...props}
+      className={classNames}
+    >
+      <path d={icons[icon]} className={styles.icon_path} />
+    </svg>
+  );
+};
 
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
